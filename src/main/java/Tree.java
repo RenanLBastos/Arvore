dcec77b301e96356959d12a45b65b45c1094ce85
RenanLBastos <renanlbastos@hotmail.com>
2021-08-30 19:38:34 -0300

    Initial commit

---
+++ src/main/java/Tree.java
@@ -0,0 +1,13 @@
+public class Tree {
+    public static void main(String[] args) {
+        //Aprensentação e printando uma árvore
+        System.out.println("Oi meu nome é Renan e eu estou aprendendo a programar em Java.");
+        System.out.println("Hoje eu vou plantar uma árvore.");
+        System.out.println("     * ** *");
+        System.out.println("     ******");
+        System.out.println("      ****");
+        System.out.println("       **");
+        System.out.println("       **");
+        System.out.println("   ~~~~~~~~~~");
+    }
+}
